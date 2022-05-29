4666c9c3ecdf8f8bfa5ce3092d366dada978b180
Aadarsha Acharya <connectwithadarsha@gmail.com>
2022-05-29 13:04:02 +0545

    feat: add selector for types of account

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,25 +1,55 @@
-import { Badge, Box, Heading, HStack, Stack, Text } from '@chakra-ui/react';
+import { AccountType } from '@/enums';
+import {
+  Badge,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import { GetServerSidePropsContext } from 'next';
 import { getSession } from 'next-auth/react';
+import * as React from 'react';
+
+const accountSelectCards = [
+  {
+    title: AccountType.PERSONAL,
+    description: 'You can use this account to store your personal information.',
+    isReleased: true,
+  },
+  {
+    title: AccountType.BUSINESS,
+    description:
+      'You can use this account to store your passwords and manage your employees',
+    isReleased: false,
+  },
+];
 
 type Props = {
-  title: string;
+  title: AccountType;
   description: string;
-  isSupported?: boolean;
+  isReleased?: boolean;
+  selectedAccountType: AccountType;
+  handleClick: (accountType: AccountType) => void;
 };
 
 function AccountSelectBox({
   title,
   description,
-  isSupported = true,
+  isReleased = true,
+  selectedAccountType,
+  handleClick,
   ...rest
 }: Props) {
+  const isCardSelected = selectedAccountType === title;
+
   return (
     <Box
       maxW="xl"
       borderWidth="1px"
       borderRadius="lg"
-      borderColor="gray.200"
       px="12"
       py="20"
       shadow="md"
@@ -30,9 +60,13 @@ function AccountSelectBox({
         borderColor: 'primary.300',
         backgroundColor: 'primary.50',
       }}
+      background={isCardSelected ? 'primary.50' : 'white'}
+      border={isCardSelected ? '1px' : 'none'}
+      borderColor={isCardSelected ? 'primary.300' : 'gray.200'}
+      onClick={() => handleClick(title)}
       {...rest}
     >
-      {!isSupported && (
+      {!isReleased && (
         <Badge
           borderRadius="full"
           colorScheme="orange"
@@ -56,6 +90,17 @@ function AccountSelectBox({
 }
 
 function Dashboard() {
+  const [selectedAccountType, setSelectedAccountType] =
+    React.useState<AccountType>(AccountType.PERSONAL);
+
+  const handleAccountCardClick = React.useCallback(
+    (accountType: AccountType) => {
+      setSelectedAccountType(accountType);
+      console.log('handleAccountCardClick', accountType);
+    },
+    [],
+  );
+
   return (
     //  From a usage standpoint, we don't recommend adding custom margins to the children of HStack, VStack or Stack.
     // If you ever need to, then maybe you don't need to use them and reach for Flex or use the shouldWrapChildren prop.
@@ -65,24 +110,39 @@ function Dashboard() {
       justifyContent="center"
       shouldWrapChildren
     >
-      <Box>
-        <Heading as="h1" size="2xl" color="gray.700">
-          Choose Account Type
-        </Heading>
-      </Box>
+      <Heading as="h1" size="2xl" color="gray.700">
+        Choose Account Type
+      </Heading>
+      <SimpleGrid spacing={8} mt={8} columns={{ base: 1, md: 2 }}>
+        {accountSelectCards.map((card, index) => (
+          <AccountSelectBox
+            key={index}
+            title={card.title}
+            description={card.description}
+            isReleased={card.isReleased}
+            handleClick={handleAccountCardClick}
+            selectedAccountType={selectedAccountType}
+          />
+        ))}
+      </SimpleGrid>
 
-      <HStack spacing={8} mt={8}>
-        <AccountSelectBox
-          title="Personal"
-          description="You can use this account to store your personal information."
-        />
-        <AccountSelectBox
-          title="Business"
-          description=" You can use this account to store your passwords and manage your
-            employees.."
-          isSupported={false}
-        />
-      </HStack>
+      <Flex flex={1}>
+        <Button
+          size="lg"
+          bg="primary.600"
+          color="white"
+          fontSize="xl"
+          px={24}
+          py={8}
+          type="submit"
+          mt={14}
+          _hover={{
+            bg: 'primary.500',
+          }}
+        >
+          Next
+        </Button>
+      </Flex>
     </Stack>
   );
 }
